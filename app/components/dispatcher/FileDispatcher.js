17fcfaa3515fcaa5a99d127950283363b9fc5ebf
Harshavardhana <harsha@harshavardhana.net>
2015-11-06 11:51:28 -0800

    cleanup FileStore,Dispatcher codebase

--- app/components/dispatcher/FileDispatcher.js
+++ app/components/dispatcher/FileDispatcher.js
@@ -1,6 +1,6 @@
-var Dispatcher = require('flux').Dispatcher;
-export var FileDispatcher = new Dispatcher();
+import { Dispatcher } from 'flux';
 
+export var FileDispatcher = new Dispatcher();
 FileDispatcher.handleAction = function(action) {
   this.dispatch({
     source: 'VIEW_ACTION',
